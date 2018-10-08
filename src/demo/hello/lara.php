a051b3be7eac8c78e0ba93951c0c8025ebe23f80
3A532092 <star1998410@gmail.com>
2018-10-08 21:19:20 +0800

    類別自動載入 lara.php

---
+++ src/demo/hello/lara.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/10/8
+ * Time: 下午 09:18
+ */
+namespace demo\hello;
+
+class Lara
+{
+    function __construct()
+    {
+        echo 'Hello Lara~'.PHP_EOL;
+    }
+}
