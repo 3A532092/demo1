9db2a7d086c249d5f01d00664cbc38e4c97571cf
3A532092 <star1998410@gmail.com>
2018-10-08 21:15:44 +0800

    類別自動載入 helloworld.php

---
+++ src/demo/helloworld.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/10/8
+ * Time: 下午 09:12
+ */
+namespace demo;
+
+class HelloWorld
+{
+    function __construct()
+    {
+        echo 'Hello World~'.PHP_EOL;
+    }
+}
